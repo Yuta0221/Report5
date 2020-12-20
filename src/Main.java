310d1e08726441fb5ed44dbc163388e67483dc7e
仲宗根悠太 <nakasoneyuuta@nakasoneyuutanoMacBook-Air.local>
2020-12-20 19:03:45 +0900

    catch NullPointerException

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
-public class Main{
-    public static void main(){
-        String str = null;
-        System.out.print(str.length());
+public class Main {
+    public static void main(String[] args) {
+        try {
+            String str = null;
+            System.out.println(str.length());
+        }
+        catch (NullPointerException e) {
+            System.out.println("NullPointerExceptionが発生した");
+            System.out.println(e.getMessage());
+        }
     }
 }
